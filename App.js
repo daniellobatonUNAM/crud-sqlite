0c3cc59c3b664af6fb145a4656f47595de741748
daniellobatonUNAM <danielmedina117@comunidad.unam.mx>
2024-01-09 16:36:56 -0600

    CRUD básico con SQLite

--- App.js
+++ App.js
@@ -1,11 +1,117 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+import * as SQLITE from 'expo-sqlite';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const db = SQLITE.openDatabase('example.db');
+  const [isLoading, setIsLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const [currentName, setCurrentName] = useState(undefined);
+
+  useEffect(() => {
+
+    db.transaction(tx => {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);');
+    });
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM names;', null, 
+      (txObj, resultSet) => setNames(resultSet.rows._array),
+      (txObj, error) => console.log(error));
+    });
+
+    setIsLoading(false);
+
+  }, []);
+
+
+  if(isLoading){
+    return <View style={styles.container}><Text>Loading names...</Text></View>
+  }
+
+  const showNames = () => {
+
+    return names.map((name, index) => {
+
+      return (
+        <View style={styles.row} key={index}>
+          <Text>{name.name}</Text>
+          <Button title='Delete' onPress={() => deleteName(name.id)}/>
+          <Button title='Update' onPress={() => updateName(name.id)}/>
+        </View>
+      )
+
+    })
+
+  }
+
+  const addName = () => {
+
+    db.transaction(tx => {
+
+      tx.executeSql('INSERT INTO names (name) VALUES (?);', [currentName],
+        (txObj, resultSet) => {
+          let existingNames = [...names];
+          existingNames.push({id: resultSet.insertId, name: currentName});
+          setNames(existingNames);
+          setCurrentName(undefined);
+        },
+        (txObj, error) => console.log(error.message)
+      );
+
+    })
+
+  }
+
+  const updateName = (id) => {
+
+    db.transaction(tx => {
+
+      tx.executeSql('UPDATE names SET name = ? WHERE id = ?;', [currentName, id],
+        (txObj, resultSet) => {
+          if(resultSet.rowsAffected > 0){
+            let existingNames = [...names];
+            const indexToUpdate = existingNames.findIndex(name => name.id === id);
+            existingNames[indexToUpdate].name = currentName;
+            setNames(existingNames);
+            setCurrentName(undefined);
+          }
+        },
+        (txObj, error) => console.log(error)
+      );
+      
+    })
+
+  }
+
+  const deleteName = (id) => {
+
+    db.transaction(tx => {
+      tx.executeSql('DELETE FROM names WHERE id = ?;', [id],
+        (txObj, resultSet) => {
+          if(resultSet.rowsAffected > 0){
+            let existingNames = [...names].filter(name => name.id !== id);
+            setNames(existingNames);
+          }
+        },
+        (txObj, error) => console.log(error.message)
+      );
+    })
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput value={currentName} placeholder='Name' onChangeText={setCurrentName}/>
+
+      <Button title='Add name' onPress={addName}/>
+
+      {showNames()}
+
     </View>
   );
 }
@@ -17,4 +123,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    margin: 8,
+  }
 });
